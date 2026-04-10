829e1751e06dc18e0adb4aec358deb95a1c8e474
kkurokawa <k.kurokawa.shineforce@gmail.com>
2026-04-10 22:48:12 +0900

    APIのエンドポイント先を本番(Render)用に修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8000" # React側はポート番号8000で作るので「localhost:8000」を指定
+    # origins "localhost:8000" # React側はポート番号8000で作るので「localhost:8000」を指定
+    origins "webapp-six-indol.vercel.app" # React側はポート番号8000で作るので「localhost:8000」を指定
 
     resource "*",
       headers: :any,
