b1b593dc4932e7baefe0d8bebe07ae004b043015
kkurokawa <k.kurokawa.shineforce@gmail.com>
2026-04-25 08:35:44 +0900

    fix: Renderデプロイエラー対応　サインアップ時のリクエスト不具合対応

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++ backend/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,5 +1,5 @@
-class Api::V1::Auth::SessionsController < ApplicationController
-  skip_before_action :authenticate_api_v1_user!, only: [:index]
+class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
+  skip_before_action :authenticate_api_v1_user!, only: [:index,:create]
 
   def index
     if current_api_v1_user
